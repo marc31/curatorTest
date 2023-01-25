f0a64680841ef8e6b00c0bb38518ef64d2be2f79
marc.foletto <marc.foletto@gmail.com>
2023-01-25 10:10:16 +0100

    build: add User seeder test@example.com 12345678

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
     }
 }
